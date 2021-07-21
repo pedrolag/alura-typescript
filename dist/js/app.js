04273eeb2af33eb61ab5db4c283ea49e127e032c
Pedro L. A. Gonçalves <pedrolagoncalves@gmail.com>
2021-07-20 22:33:10 -0300

    criação do modulo negociacao

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,13 @@
-alert('hello');
+import { Negociacao } from './models/negociacao.js';
+
+const negociacao = new Negociacao(
+    new Date(),
+    10,
+    100
+);
+
+console.log(negociacao);
+
+negociacao.quantidade = 1000;
+
+console.log(negociacao);
