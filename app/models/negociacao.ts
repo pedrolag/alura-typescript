feff942b00509582cb63f87938977129a3b15828
Pedro L. A. Gonçalves <pedrolagoncalves@gmail.com>
2021-07-26 19:03:36 -0300

    blindando a propriedade 'data' da classe negociacao

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,9 +1,15 @@
 export class Negociacao {
     constructor(
-        public readonly data: Date,
+        private _data: Date,
         public readonly quantidade: number,
         public readonly valor: number
     ) {}
+
+    get data(): Date {
+        // Desta forma não é possível alterar a propriedade data
+        // através de negociacao.data.setDate()
+        return new Date(this._data.getTime());
+    }
 
     get volume(): number {
         return this.quantidade * this.valor;
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,8 +1,13 @@
 export class Negociacao {
-    constructor(data, quantidade, valor) {
-        this.data = data;
+    constructor(_data, quantidade, valor) {
+        this._data = _data;
         this.quantidade = quantidade;
         this.valor = valor;
+    }
+    get data() {
+        // Desta forma não é possível alterar a propriedade data
+        // através de negociacao.data.setDate()
+        return new Date(this._data.getTime());
     }
     get volume() {
         return this.quantidade * this.valor;
